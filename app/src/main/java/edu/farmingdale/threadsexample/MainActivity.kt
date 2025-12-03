72a76fa5f32ec396fbc29b2a4925d19b6b81f504
Brenda Galdamez <jgaldamez098@gmail.com>
2025-12-02 20:07:31 -0500

    // ToDo 1: Call `FibonacciDemoNoBgThrd` that calculates the Fibonacci number of a given number.

--- app/src/main/java/edu/farmingdale/threadsexample/MainActivity.kt
+++ app/src/main/java/edu/farmingdale/threadsexample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,7 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             ThreadsExampleTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting("Android", Modifier.padding(innerPadding))
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        FibonacciDemoNoBgThrd()
+                    }
                 }
             }
         }
